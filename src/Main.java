19007686a5415e414c32c53cda472a73846677fa
HunterYarmy <huntery5@nycstudents.net>
2025-09-17 15:34:57 -0400

    Added two classes, some variables, and print statements

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        DogWalkCompany c = new DogWalkCompany();
+        System.out.println(c);
+        int hour = c.addDogs();
+        System.out.println(c);
+    }
+}
